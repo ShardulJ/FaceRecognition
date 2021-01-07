3136a2e789321b94eb701514c4bf7d649f61cc07
SHARDUL JANASKAR <sharduljanaskar@SHARDULs-MacBook-Air.local>
2021-01-07 18:42:19 +0530

    changes to model

--- model/net_s3fd.py
+++ model/net_s3fd.py
@@ -3,21 +3,6 @@ import torch.nn as nn
 from torch.autograd import Variable
 import torch.nn.functional as F
 
-class L2Norm(nn.Module):
-    def __init__(self,n_channels, scale=1.0):
-        super(L2Norm,self).__init__()
-        self.n_channels = n_channels
-        self.scale = scale
-        self.eps = 1e-10
-        self.weight = nn.Parameter(torch.Tensor(self.n_channels))
-        self.weight.data *= 0.0
-        self.weight.data += self.scale
-
-    def forward(self, x):
-        norm = x.pow(2).sum(dim=1, keepdim=True).sqrt()+self.eps
-        x = x / norm * self.weight.view(1,-1,1,1)
-        return x
-
 class s3fd(nn.Module):
     def __init__(self, num_classes, batch_size):
         
